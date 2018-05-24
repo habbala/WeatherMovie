3ef4be715e1eb44b1edcaab91244d09dd8e97d56
Johanna Hermansson <id15jhn@cs.umu.se>
2018-05-24 16:49:39 +0200

    inget bra

--- src/components/MovieClient/index.js
+++ src/components/MovieClient/index.js
@@ -4,21 +4,23 @@ import './index.css';
 
 export default class Movie extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      movieName: '',
+    }
+  }
+
+  
+
   render(){
     const imdb = require('imdb-api');
-    imdb.get('The Toxic Avenger', {apiKey: '2ab7eeb0', timeout: 30000}).then(console.log).catch(console.log);
-
-    //var weather = require('openweather-apis');
-    //weather.setAPPID('5a8b6837143c58aa94dee4d3be632930');
-    //weather.setLang('en');
-    //weather.setCity('Stockholm');
-    /*weather.getAllWeather(function(err, JSONObj){
-      console.log(JSONObj);
-    });*/
+    const movieInfo = imdb.get('The Toxic Avenger', {apiKey: '2ab7eeb0', timeout: 30000}).then(console.log).catch(console.log);
 
     return (
-      <div>
+      <div className="movieContainer">
         hello3
+
       </div>
     );
   }
