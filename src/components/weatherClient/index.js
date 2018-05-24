46a1209100de3650a66f63c6391f2967134825d8
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-05-24 17:07:14 +0200

    made getWeather clickable

--- src/components/weatherClient/index.js
+++ src/components/weatherClient/index.js
@@ -8,26 +8,34 @@ export default class Weather extends Component {
   constructor(props){
     super(props);
     this.state = {
-      weatherInfo: '',
+      weatherInfo: 0,
     }
+    this.eventHandler = this.eventHandler.bind(this);
   }
 
   componentWillMount(){
     weather.setAPPID('5a8b6837143c58aa94dee4d3be632930');
     weather.setLang('en');
     weather.setCity('Stockholm');
+    this.setState({weatherInfo: this.state.weatherInfo + 1});
+  }
+
+  getWeather(){
     weather.getAllWeather(function(err, JSONObj){
-      console.log(JSONObj);
+      console.log(JSONObj.main);
     });
 
-    this.setState({weatherInfo: 'hej'});
+    this.setState({weatherInfo: this.state.weatherInfo + 1});
 
   }
 
-  render() {
+  eventHandler(event){
+    this.getWeather();
+  }
 
+  render() {
     return (
-      <div className="weather-container">
+      <div className="weather-container" onClick={this.eventHandler}>
         {this.state.weatherInfo}
       </div>
     );
