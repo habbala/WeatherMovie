c5d6ef22f3ab3a483a14e79ee6f860f3cb1c3fad
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-05-24 20:05:06 +0200

    Weather now gets weather and prints it!

--- src/components/weatherClient/index.js
+++ src/components/weatherClient/index.js
@@ -8,8 +8,9 @@ export default class Weather extends Component {
   constructor(props){
     super(props);
     this.state = {
-      weatherInfo: 0,
+      weatherInfo: '',
     }
+    this.getWeather = this.getWeather.bind(this);
     this.eventHandler = this.eventHandler.bind(this);
   }
 
@@ -17,16 +18,17 @@ export default class Weather extends Component {
     weather.setAPPID('5a8b6837143c58aa94dee4d3be632930');
     weather.setLang('en');
     weather.setCity('Stockholm');
-    this.setState({weatherInfo: this.state.weatherInfo + 1});
+  }
+
+  componentDidMount(){
+    this.getWeather();
   }
 
   getWeather(){
-    weather.getAllWeather(function(err, JSONObj){
-      console.log(JSONObj.main);
+    weather.getAllWeather((err, response) => {
+      console.log(response);
+      this.setState({weatherInfo: response.weather[0].main});
     });
-
-    this.setState({weatherInfo: this.state.weatherInfo + 1});
-
   }
 
   eventHandler(event){
