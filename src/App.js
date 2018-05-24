986f092f15548428822956c6f145cc5c773f1290
Johanna Hermansson <id15jhn@cs.umu.se>
2018-05-24 16:29:44 +0200

    component Movie funkar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './index.css';
 import Weather from './components/weatherClient';
+import Movie from './components/MovieClient';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Weather/>
+        <Movie/>
       </div>
     );
   }
--- src/components/MovieClient/index.js
+++ src/components/MovieClient/index.js
@@ -1,12 +1,25 @@
 //IMDB-API key: 2ab7eeb0
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import './index.css';
 
 export default class Movie extends Component{
 
   render(){
     const imdb = require('imdb-api');
-    return();
+    imdb.get('The Toxic Avenger', {apiKey: '2ab7eeb0', timeout: 30000}).then(console.log).catch(console.log);
+
+    //var weather = require('openweather-apis');
+    //weather.setAPPID('5a8b6837143c58aa94dee4d3be632930');
+    //weather.setLang('en');
+    //weather.setCity('Stockholm');
+    /*weather.getAllWeather(function(err, JSONObj){
+      console.log(JSONObj);
+    });*/
+
+    return (
+      <div>
+        hello3
+      </div>
+    );
   }
 }
