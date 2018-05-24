ae165218826e72031eb7f7b7806665f792e907b5
Johanna Hermansson <id15jhn@cs.umu.se>
2018-05-24 16:20:30 +0200

    component movie

---
+++ src/components/MovieClient/index.js
@@ -0,0 +1,12 @@
+//IMDB-API key: 2ab7eeb0
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export default class Movie extends Component{
+
+  render(){
+    const imdb = require('imdb-api');
+    return();
+  }
+}
