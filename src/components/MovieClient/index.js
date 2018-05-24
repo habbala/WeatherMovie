3f5c756448c4f1f5e6895c522af567d050c23926
Johanna Hermansson <id15jhn@cs.umu.se>
2018-05-24 17:08:59 +0200

    positionering

--- src/components/MovieClient/index.js
+++ src/components/MovieClient/index.js
@@ -11,16 +11,25 @@ export default class Movie extends Component{
     }
   }
 
-  
-
-  render(){
+  componentWillMount(){
     const imdb = require('imdb-api');
     const movieInfo = imdb.get('The Toxic Avenger', {apiKey: '2ab7eeb0', timeout: 30000}).then(console.log).catch(console.log);
+    this.setState({movieName: ' bajs'});
+  }
 
+  render(){
     return (
       <div className="movieContainer">
-        hello3
-
+        <div className="movieTitle">
+          {this.state.movieName}
+        </div>
+        <div className="moviePoster">
+          fin bild på postern
+        </div>
+        <div className="movieInformation">
+          <p>Rated: </p>
+          <p>Description:</p>
+        </div>
       </div>
     );
   }
